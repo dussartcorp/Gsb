29232252ef263bad1f1f23b9b7126add9856a7c0
Thibault <laudethibault_pro@outlook.fr>
2020-11-10 14:32:56 +0100

    Update GSB_modifications_table.sql

--- database/GSB_modifications_table.sql
+++ database/GSB_modifications_table.sql
@@ -14,5 +14,5 @@ update visiteur set mdp = SHA2(mdp, 512);
 insert into comptable(id,nom,prenom,login,mdp) values 
 ('c01','AHMED ALI','Nassim','Anas', SHA2('P@ssw0rd', 512)),
 ('c02', 'LAUDE', 'Thibault', 'Lthi', SHA2('P@ssw0rd', 512)),
-('c02', 'DUSSART', 'Luke', 'Dluke', SHA2('P@ssw0rd', 512));
+('c03', 'DUSSART', 'Luke', 'Dluke', SHA2('P@ssw0rd', 512));
 select * from visiteur;

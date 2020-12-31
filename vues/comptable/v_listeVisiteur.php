54b69436e4119725d1e97cbe5961370485642c9d
DUSSARTLuke <65154957+DUSSARTLuke@users.noreply.github.com>
2020-12-31 11:48:18 +0100

    change fin tache 2

--- vues/comptable/v_listeVisiteur.php
+++ vues/comptable/v_listeVisiteur.php
@@ -46,20 +46,19 @@
       <h3>Sélectionner un visiteur : </h3>
     </div>
     <div class="col-md-4">
-      <form action="index.php?uc=suivrePaiementFrais&action=afficheFiche" 
+      <form action="index.php?uc=suivrePaiementFrais&action=FicheFraisSP" 
             method="post" role="form">
         <div class="form-group">
-          <label for="lstVisiteurs" accesskey="n">Visiteur : </label>
-          <select id="lstVisiteurs" name="lstVisiteurs" class="form-control">
+          <label for="lstVisiteur" accesskey="n">Visiteur : </label>
+          <select id="lstVisiteur" name="lstVisiteur" class="form-control">
             <?php
             foreach ($lesVisiteurs as $unVisiteur) {
-              $id_vst = $unVisiteur['id'];
-              $nom_vst = $unVisiteur['nom'];
-              $prenom_vst = $unVisiteur['prenom'];
-              ?> 
-              <option selected value="<?php echo $id_vst ?>">
-                <?php echo $nom_vst . ' ' . $prenom_vst ?> </option> 
-              <?php
+              $id_vst = $unVisiteur['visiteur'];
+              $nom_vst = $unVisiteur['nomvisiteur'];
+              if ($selectedValue == $id_vst) {
+                ?><option selected value="<?php echo $selectedValue ?>"><?php echo $unVisiteur['nomvisiteur'] ?></option>               
+              <?php } else { ?> <option value="<?php echo $id_vst ?>"><?php echo $unVisiteur['nomvisiteur'] ?></option> <?php
+              }
             }
             ?>   
           </select>     
@@ -71,5 +70,4 @@
       </form> 
     </div>
   </div>
-<?php } ?>
-
+<?php } 

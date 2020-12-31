8b689c2e2bb460bd459c372c11368c4466299c91
DUSSARTLuke <65154957+DUSSARTLuke@users.noreply.github.com>
2020-12-31 11:49:28 +0100

    change fin tache 2

--- controleurs/c_suivrePaiementFrais.php
+++ controleurs/c_suivrePaiementFrais.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,27 +6,43 @@
  */
 
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
-$idcomptable = $_SESSION['idComptable'];
+$idVisiteur = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
+$montants = 0;
 $date = date('Ym');
 switch ($action) {
-  case 'selectionnerSuiviV':
-    $_SESSION['date'] = $date;
-    $lesVisiteurs = $pdo->getListeVisiteurVA($date);
+  case 'selectionnerSuiviVisiteur':
+    if (empty($pdo->getVisiteurFromMoisVA($date))) {
+      ?></br><?php
+      ajouterErreur("Aucun visiteur n'a de fiche de frais ce mois ci");
+      include 'vues/v_erreurs.php';
+      include 'vues/comptable/v_listeVisiteur.php';
+    } else {
+      $_SESSION['date'] = $date;
+      $lesVisiteurs = $pdo->getVisiteurFromMoisVA($date);
+      $selectedValue = $lesVisiteurs[0];
+      include 'vues/comptable/v_listeVisiteur.php';
+    }
+    break;
+  case 'FicheFraisSP':
+    $idVisi = filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING);
+    $lesVisiteurs = $pdo->getVisiteurFromMoisVA($_SESSION['date']);
+    $selectedValue = $idVisi;
     include 'vues/comptable/v_listeVisiteur.php';
-    break;
-  case 'afficheFiche':
-    $id_vst = filter_input(INPUT_POST, "lstVisiteurs");
-    $_SESSION['idVisiteur'] = $id_vst;
-    $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($_SESSION['idVisiteur'], $_SESSION['date']);
-    $lesFraisForfait = $pdo->getLesFraisForfait($_SESSION['idVisiteur'], $_SESSION['date']);
-    $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($_SESSION['idVisiteur'], $_SESSION['date']);
-    $numAnnee = substr($_SESSION['date'], 0, 4);
-    $numMois = substr($_SESSION['date'], 4, 2);
-    $lesFiches = $lesInfosFicheFrais;
-    $libEtat = $lesInfosFicheFrais['libEtat'];
-    $montantValide = $lesInfosFicheFrais['montantValide'];
-    
-    $dateModif = dateAnglaisVersFrancais($lesInfosFicheFrais['dateModif']);
+    $nomVis = (filter_input(INPUT_POST, 'lstVisiteur', FILTER_SANITIZE_STRING));
+    trim($nomVis);
+    $_SESSION['visiteur'] = $idVisi;
+    $infoFicheDeFrais = $pdo->getLesInfosFicheFrais($_SESSION['visiteur'], $_SESSION['date']);
+    $infoFraisForfait = $pdo->getLesFraisForfait($_SESSION['visiteur'], $_SESSION['date']);
+    $infoFraisHorsForfait = $pdo->getLesFraisHorsForfait($_SESSION['visiteur'], $_SESSION['date']);
     include 'vues/comptable/v_etatFraisComptable.php';
+    $_SESSION['montant'] = $montants;
     break;
+  case 'Valider' :
+    $pdo->validerFicheDeFraisVA($_SESSION['visiteur'], $_SESSION['date'], $_SESSION['montant']);
+    ?>
+    <div class = "alert alert-success" role = "alert">
+      <p>Votre fiche de frais a bien été mise en paiement ! <a href = "index.php?uc=suivrePaiementFrais&action=selectionnerSuiviVisiteur">Cliquez ici</a>
+        pour revenir à la selection.</p>
+    </div>
+  <?php
 }
